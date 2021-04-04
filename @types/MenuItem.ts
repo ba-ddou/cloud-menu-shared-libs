d7833479e1b5afa88c70920585f749a5a795e9de
abdelbassite badou <abdelbassite.badou@gmail.com>
2021-04-04 16:20:48 +0100

    updated the Owner interface ( replaced _id with id + removed username) | removed the ownerDocument interface ( document interfaces should defined on the api )

--- @types/MenuItem.ts
+++ @types/MenuItem.ts
@@ -1,5 +1,6 @@
 
 export type MenuItem = {
+    _id: string
     name: string
     price: number
     description: string
--- @types/Owner.ts
+++ @types/Owner.ts
@@ -1,16 +1,10 @@
 
 
 export interface Owner{
-    _id: string
+    id: string
     name: string
     email: string
     phone: string
     businesses: string[]
-    username: string
 
-}
-
-
-export interface OwnerDocument extends Owner {
-    hashedPassword: string
 }
