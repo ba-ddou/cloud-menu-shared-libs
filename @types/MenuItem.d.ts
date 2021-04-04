74a2d6c80da3c6b6ad7b2da484af80640d9adf1d
abdelbassite badou <abdelbassite.badou@gmail.com>
2021-04-04 16:21:10 +0100

    rebuils

--- @types/MenuItem.d.ts
+++ @types/MenuItem.d.ts
@@ -1,4 +1,5 @@
 export declare type MenuItem = {
+    _id: string;
     name: string;
     price: number;
     description: string;
--- @types/Owner.d.ts
+++ @types/Owner.d.ts
@@ -1,11 +1,7 @@
 export interface Owner {
-    _id: string;
+    id: string;
     name: string;
     email: string;
     phone: string;
     businesses: string[];
-    username: string;
-}
-export interface OwnerDocument extends Owner {
-    hashedPassword: string;
 }
