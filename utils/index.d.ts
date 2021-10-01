02e79b2c7df8d1bb456d94da13cedaeba1e6365e
Badou Abdelbassite <a.badou@sanadtech.com>
2021-10-02 00:48:26 +0100

    rebuild

--- utils/index.d.ts
+++ utils/index.d.ts
@@ -1,2 +1,1 @@
 export * from './qrcode';
-export * from './test';
--- utils/index.js
+++ utils/index.js
@@ -11,4 +11,3 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 exports.__esModule = true;
 __exportStar(require("./qrcode"), exports);
-__exportStar(require("./test"), exports);
